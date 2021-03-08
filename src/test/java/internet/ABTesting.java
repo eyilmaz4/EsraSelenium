aac566b83c57b4d22407298a93e32a54973ae5a9
eyilmaz4 <eyilmaz4@na.edu>
2021-03-08 11:12:10 -0600

    commit

--- src/test/java/internet/ABTesting.java
+++ src/test/java/internet/ABTesting.java
@@ -3,13 +3,37 @@ package internet;
 import base.BaseClass;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ABTesting extends BaseClass {
-    @Test
+    Private_Variables obj=new Private_Variables();
+    @Test(enabled = false)
     public void testABTesting(){
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.xpath("//*[text()='A/B Testing']")).click();
         Assert.assertEquals(driver.getTitle(),"The Internet");
+    }
+    @Test(dataProvider = "dataGiven")
+    public void callVariable(String name, String email, String status, String gender){
+        obj.setName(name);
+        obj.setEmail(email);
+        obj.setStatus(status);
+        obj.setGender(gender);
+
+        //Question: how can we write all data in one data table?
+//        List<String>arr=new LinkedList<>();
+//        arr.add(obj.toString());
+//        System.out.println(arr);
+        //System.out.println(obj);
+
+    }
+    @DataProvider
+    public Object[][] dataGiven(){
+        Object[][] data={{"Chen U", "chen@gmail.com","Active", "Male"},{"Tamara","t@gmail.com", "Acvite", "Female"},{"Sena","s@gmail.com","Active", "Female"}};
+        return data;
     }
 }
--- src/test/java/internet/Private_Variables.java
+++ src/test/java/internet/Private_Variables.java
@@ -1,4 +1,51 @@
 package internet;
 
 public class Private_Variables {
+    private String name;
+    private String email;
+    private String status;
+    private String gender;
+
+    @Override
+    public String toString() {
+        return "Private_Variables{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", status='" + status + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
+
